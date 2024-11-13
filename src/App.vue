5d31ed5d75f25073c97d775453943b954cb96009
Boris Milojević <45460628+Bokayy@users.noreply.github.com>
2024-11-13 01:25:20 +0100

    static site mockup

---
+++ src/App.vue
@@ -0,0 +1,54 @@
+<script setup lang="ts">
+</script>
+
+<template>
+
+  <header>
+    <h1>Anketa.hr</h1>
+  </header>
+
+  <div class="divider">
+      &nbsp;
+  </div>
+
+  <main>
+    <div class="background"></div>
+    <div class="background-2"></div>
+    <div class="card">
+      <div class="title-container">
+        <h2> TKO IMA VEĆEG???? </h2>
+      </div>
+      <div class="divider">
+        &nbsp;
+      </div>
+      <div class="survey-container">
+        <div class="survey">
+          <div class="left"></div>
+          <div class="right"></div>
+          <div class="name name1">
+            <p>Milanović</p>
+          </div>
+          <div class="name name2">
+            <p>Plenković</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </main>
+
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
